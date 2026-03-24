e04ed6693fe3f26fd0b195645a0e22819493b9d1
Mandisa Khumalo <A0070215@wits.ac.za>
2026-03-24 18:01:07 +0200

    new commit

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.urls import path, include
 
 
 def home(request):
-    return HttpResponse("HIVE-TECH-PROJECT backend is running")
+    return redirect('/admin/')
 
 
 urlpatterns = [
